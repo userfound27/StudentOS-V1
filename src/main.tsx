21865f9632a3f22f2d5133cc591fa2d10e340a55
Anant Rao <raoanant27@gmail.com>
2026-09-26 16:12:24 +0530

    Check for newer service worker on app startup

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import "./styles.css";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
+    navigator.serviceWorker.register("/sw.js").then(registration=>{
+      registration.update().catch(()=>{});
+    }).catch(() => {});
   });
 }
 
